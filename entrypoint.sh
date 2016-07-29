b10d8417ffdb1a0ada8b2c3cd5fd11f44a6dae94
cristina-grosu <cristina.grosu@bigstep.com>
2016-07-29 16:59:34 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,6 +42,7 @@ fi
 sed "s/HOSTNAME_MASTER/$SPARK_MASTER_HOSTNAME/" /opt/spark-2.0.0-bin-hadoop2.7/conf/spark-defaults.conf.template > /opt/spark-2.0.0-bin-hadoop2.7/conf/spark-defaults.conf
 #sed "s/HOSTNAME/$SPARK_MASTER_HOSTNAME/" /opt/spark-2.0.0-bin-hadoop2.7/conf/core-site.xml.template > /opt/spark-2.0.0-bin-hadoop2.7/conf/core-site.xml
 sed "s/HOSTNAME/$SPARK_MASTER_HOSTNAME/" /opt/spark-2.0.0-bin-hadoop2.7/conf/yarn-site.xml.template > /opt/spark-2.0.0-bin-hadoop2.7/conf/yarn-site.xml	
+sed "s/HOSTNAME/$SPARK_MASTER_HOSTNAME/" /opt/spark-2.0.0-bin-hadoop2.7/conf/mapred-site.xml.template > /opt/spark-2.0.0-bin-hadoop2.7/conf/mapred-site.xml	
 
 SPARK_MASTER_URL="spark://$SPARK_MASTER_HOSTNAME:$SPARK_MASTER_PORT"
 echo "Using SPARK_MASTER_URL=$SPARK_MASTER_URL"
