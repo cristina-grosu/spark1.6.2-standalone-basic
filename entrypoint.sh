ddc558dd1382f727ae82fa530b3f346ede9d4ffb
cristina-grosu <cristina.grosu@bigstep.com>
2016-08-02 17:24:32 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -52,7 +52,7 @@ fi
 
 if [ "$MODE" == "master" ]; then 
 	${SPARK_HOME}/bin/spark-class "org.apache.spark.deploy.master.Master" --ip $SPARK_MASTER_IP --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT &
-	notebook jupyter --ip=0.0.0.0
+	jupyter notebook --ip=0.0.0.0
 
 elif [ "$MODE" == "worker" ]; then
 	${SPARK_HOME}/bin/spark-class "org.apache.spark.deploy.worker.Worker" --webui-port $SPARK_WORKER_WEBUI_PORT --port $SPARK_WORKER_PORT $SPARK_MASTER_URL -c $CORES -m $MEM
