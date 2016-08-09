a7ace5ad36cefee4c950a8ef1f7f3ef2853f3bed
cristina-grosu <cristina.grosu@bigstep.com>
2016-08-09 18:27:20 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SPARK_HOME="/opt/spark-2.0.0-bin-hadoop2.7"
+SPARK_HOME="/opt/spark-1.6.2-bin-hadoop2.6"
 
 echo Using SPARK_HOME=$SPARK_HOME
 
@@ -41,13 +41,10 @@ if [ "$SPARK_MASTER_HOSTNAME" = "" ]; then
 fi
 
 
-sed "s/HOSTNAME_MASTER/$SPARK_MASTER_HOSTNAME/" /opt/spark-2.0.0-bin-hadoop2.7/conf/spark-defaults.conf.template > /opt/spark-2.0.0-bin-hadoop2.7/conf/spark-defaults.conf
+sed "s/HOSTNAME_MASTER/$SPARK_MASTER_HOSTNAME/" /opt/spark-1.6.2-bin-hadoop2.6/conf/spark-defaults.conf.template > /opt/spark-1.6.2-bin-hadoop2.6/conf/spark-defaults.conf
 
 SPARK_MASTER_URL="spark://$SPARK_MASTER_HOSTNAME:$SPARK_MASTER_PORT"
 echo "Using SPARK_MASTER_URL=$SPARK_MASTER_URL"
-
-git config --global user.name  $GIT_USER && \
-git config --global user.email $GIT_EMAIL
 
 if [ "$MODE" = "" ]; then
 MODE=$1
@@ -56,7 +53,6 @@ fi
 if [ "$MODE" == "master" ]; then 
 	${SPARK_HOME}/bin/spark-class "org.apache.spark.deploy.master.Master" --ip $SPARK_MASTER_IP --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT &
 	jupyter notebook --ip=0.0.0.0 
-	#--NotebookApp.server_extensions="['github_commit_push']"
 
 elif [ "$MODE" == "worker" ]; then
 	${SPARK_HOME}/bin/spark-class "org.apache.spark.deploy.worker.Worker" --webui-port $SPARK_WORKER_WEBUI_PORT --port $SPARK_WORKER_PORT $SPARK_MASTER_URL -c $CORES -m $MEM
@@ -64,5 +60,4 @@ else
 	${SPARK_HOME}/bin/spark-class "org.apache.spark.deploy.master.Master" --ip $SPARK_MASTER_IP --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT &
 	${SPARK_HOME}/bin/spark-class "org.apache.spark.deploy.worker.Worker" --webui-port $SPARK_WORKER_WEBUI_PORT --port $SPARK_WORKER_PORT $SPARK_MASTER_URL	-c $CORES -m $MEM &
 	jupyter notebook --ip=0.0.0.0 
-	#--NotebookApp.server_extensions="['github_commit_push']"
 fi
